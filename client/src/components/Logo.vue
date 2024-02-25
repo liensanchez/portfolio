09717d9f118944328f2d9d3ec89ef47b16fec4e2
liendev <liens@alphaefficiency.me>
2024-02-25 02:26:57 -0300

    navbar desktop and mobile

---
+++ client/src/components/Logo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="logo-display">
+    <svg
+      width="30px"
+      height="30px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.49997 12.4L5 17.7999"
+        stroke="#F6F8FC"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.49997 12.4L5 7"
+        stroke="#F6F8FC"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <line
+        x1="18"
+        y1="18"
+        x2="10"
+        y2="18"
+        stroke="#F6F8FC"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+    <p>Liendev</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.logo-display {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+}
+</style>
